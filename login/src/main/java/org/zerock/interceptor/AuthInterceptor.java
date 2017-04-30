8e8b4dd8e1060efa41824b3ddc3a0225b841f506
bbako <hoyeon1008@gmail.com>
2017-04-30 18:01:46 +0900

    2

--- login/src/main/java/org/zerock/interceptor/AuthInterceptor.java
+++ login/src/main/java/org/zerock/interceptor/AuthInterceptor.java
@@ -4,9 +4,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class AuthInterceptor extends HandlerInterceptorAdapter{
+	
+	static final Logger logger = Logger.getLogger(AuthInterceptor.class);
+	
+	private void saveDest(HttpServletRequest req){
+		
+		String uri = req.getRequestURI();
+		String query = req.getQueryString();
+		
+		if (query == null || query.equals("null")) {
+			query = "";
+			
+		}else{
+			query = "?" + query;
+		}
+		
+		if(req.getMethod().equals("GET")){
+			
+			logger.info("dest: " + (uri + query));
+			req.getSession().setAttribute("dest",uri +query);
+		}
+		
+	}
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
@@ -17,12 +40,14 @@ public class AuthInterceptor extends HandlerInterceptorAdapter{
 		
 		if(session.getAttribute("login")==null){
 			
+			logger.info("current user is not logined");
+			
+			saveDest(request);
+			
 			response.sendRedirect("/user/login");
 			
 			return false;
 		}
-		
-		
 		
 		
 		return true;
